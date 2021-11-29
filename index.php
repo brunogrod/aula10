562743d50fbf16b13500306c020c64e7c80bb856
brunogrod <brunogrod@gmail.com>
2021-11-29 20:47:50 -0300

    versão 0.0.1 - Aula 10

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Controle</title>
+</head>
+<body>
+        Menu Principal
+        <hr>
+        <a href="frmclientes.php"> Clientes
+        <a href="frmprodutos.php"> Produtos
+        <a href="frmvendedores.php"> Vendedores
+        <a href="frmvendas.php"> Vendas 
+</body>
+</html>
